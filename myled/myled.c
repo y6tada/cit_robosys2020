98258c5a62e4430a4d08243b8ff6e610e609411a
WdC7qu <y6tada.home@gmail.com>
2020-11-30 01:00:22 +0900

    add device info and confirm modinfo

--- myled/myled.c
+++ myled/myled.c
@@ -1,12 +1,18 @@
 #include <linux/module.h>
+MODULE_AUTHOR("Yusuke Tada and Ryuichi Ueda");
+MODULE_AUTHOR("Driver for LED control, source code originally from lecture cit_robosys2020 organized by Prof.Ryuichi Ueda");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.0.1");
 
 static int __init init_mod(void)
 {
-    return 0;
+    printk(KERN_INFO "%s is loade. \n", __FILE__);
+	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
+	printk(KERN_INFO "%s is unloaded.\n", __FILE__);
 }
 
 module_init(init_mod);
