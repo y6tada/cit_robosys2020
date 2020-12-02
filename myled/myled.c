d59c884bac1fc580a295d596e9e25d99e2b023a3
WdC7qu <y6tada.home@gmail.com>
2020-12-02 20:07:28 +0900

    gpio on-off worked

--- myled/myled.c
+++ myled/myled.c
@@ -3,6 +3,10 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+#include <linux/io.h>
+
+// Raspberry pi 4
+#define RPI_REG_BASE (0xFE200000)
 
 MODULE_AUTHOR("Yusuke Tada and Ryuichi Ueda");
 MODULE_AUTHOR("Driver for LED control, source code originally from lecture cit_robosys2020 organized by Prof.Ryuichi Ueda");
@@ -12,6 +16,7 @@ MODULE_VERSION("0.0.1");
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t *pos)
 {
@@ -20,7 +25,15 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 		return -EFAULT;
 	}
 
-	printk(KERN_INFO "recieve %c\n", c);
+	// printk(KERN_INFO "recieve %c\n", c);
+
+	if (c =='0') {
+		gpio_base[10] = 1 << 25;
+	}
+	else if (c == '1') {
+		gpio_base[7] = 1 << 25;
+	}
+
 	return 1;
 }
 
@@ -55,17 +68,25 @@ static int __init init_mod(void)
 
 	cdev_init(&cdv, &led_fops);
 	retval = cdev_add(&cdv, dev, 1);
-	if(retval < 0) {
+	if (retval < 0) {
 		printk(KERN_ERR "cdev_add failed. major:%d, manor:%d\n", MAJOR(dev), MINOR(dev));
 	}
 
 	cls = class_create(THIS_MODULE, "myled");
-	if(IS_ERR(cls)) {
+	if (IS_ERR(cls)) {
 		printk(KERN_ERR, "class create failed.");
 		return PTR_ERR(cls);
 	}
 
 	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
+	
+	gpio_base = ioremap(RPI_REG_BASE, 0xA0);
+
+	const u32 led 		= 25;
+	const u32 index 	= led / 10;
+	const u32 shift 	= (led%10)*3;
+	const u32 mask  	= ~(0x7 << shift);
+	gpio_base[index] 	= (gpio_base[index] & mask) | (0x1 << shift);
 
 	return 0;
 }
