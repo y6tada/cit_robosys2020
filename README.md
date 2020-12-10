39b82a1236531c8aba6e5f52bde192419e0c126c
y6tada <y6tada.cnc@gmail.com>
2020-12-10 22:08:21 +0900

    Update typo, add hardware description

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # cit_robosys2020
+家電の遠隔操作に使われるIRリモコンの信号読み取りと吐き出しを行うマイコンに指令（Digital pinのon-->off)を行うデバイスドライバです。
+
 CITロボットシステム学 講義用リポジトリ  
 Original lecture: [ryuichiueda/robosys2020](https://github.com/ryuichiueda/robosys2020)
 
 ## Video
 [![](https://img.youtube.com/vi/n9D5KT2ge2s/0.jpg)](https://www.youtube.com/watch?v=n9D5KT2ge2s)
 
-## 説明
-家電の遠隔操作に使われるIRリモコンの信号読み取りと吐き出しをして、家電のリモコン操作をRasPiから行えるようにする。
-IRリモコンの信号は38〜40kHzで変調された信号にパルスの信号をかぶせてあるので、PWMで変調を行い、信号はPWMの吐き出しタイミングを制御すること行う。
+## 動作環境
+* Raspberry Pi 4 Model B 4GB (Ubuntu 20.04 server)
+* MacBook Pro 16-inch Model A2141 (macOS Big Sur Version 11.0.1)
 
-### マイコンの処理
+## 説明
+#### ハードウェア
+||型番|
+|---|---|
+|赤外線LED|OS15LA5113A|
+|赤外線センサ|TSSP58038|
+![Picture1](https://user-images.githubusercontent.com/18658190/101752185-8ca5e880-3b14-11eb-8d14-75c3c361ec0a.png)
+
+#### マイコンの処理
 マイコンで行われるのは以下のシンプルな順序処理、詳細は`FRDM_K64F/main.cpp`を参照。
 
 1. IR信号を検出するまで待機
@@ -24,22 +34,21 @@ IR信号の読み込み結果は処理過程でシリアルプリントされる
 
 ![ir_signal_log](https://user-images.githubusercontent.com/18658190/101500087-5865e700-39b1-11eb-8f58-01ad96c528a1.png)
 
-### オリジナルから変更が加えられた箇所
+#### オリジナルから変更が加えられた箇所
 講義で扱われたドライバから加えられた変更は以下の2つのみ。
 最初はRasPiのGPIOをタイマーベースで制御し、数usオーダーのピン操作が可能なのか調べたが、どうやら厳しいという結論に至る。
 
 1. myledモジュールに`p = pulse`のモードを追加
 1. タイマーでGPIOを落とす(200ms待機)
 
-### 
-
-## 実行とログ確認
-カーネルモジュールのインストール
-
-1. `myled/`下で`make`
-1. `sudo insmod myled.ko`
-1. `sudo chmod 666 /dev/myled`
-
+## インストール
+```
+git clone cit_robosys2020
+cd cit_robosys2020/myled
+make
+sudo insmod myled.ko
+sudo chmod 666 /dev/myled
+```
 デバイスに文字列`'1'`を渡す
 ```
 echo 1 > /dev/myled
@@ -66,7 +75,8 @@ myled.c:75:14: error: implicit declaration of function ‘ioremap_nocache’; di
 ```
 5.8.0-1007-raspi
 ```
-仕方が無いので`ioremap()`でとりあえず進めた。i
+仕方が無いので`ioremap()`でとりあえず進めた。
+
 ### `linux/hrtimer`のタイマーハンドラの戻り値
 `myled.c`の`static enum hrtimer_restart timer_handler(struct hrtimer *_timer)`は以下で定義されるenumの戻り値を返すが、これに引数を指定しないOR`HRTIMER_RESTART`を返すと暴走する。
 ```
@@ -76,4 +86,6 @@ enum hrtimer_restart {
 };
 ```
 
-
+## COPYING
+本リポジトリはGPLライセンスで公開されています。
+詳細は[COPYING](./COPYING)を参照して下さい。
--- myled/myled.c
+++ myled/myled.c
@@ -37,28 +37,28 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 
 typedef enum  {
-	published,
-	edited
+    published,
+    edited
 } publish_status_t;
 
 typedef enum {
-	on,
-	off,
-	pulse
+    on,
+    off,
+    pulse
 } gpio_status_t;
 
 static publish_status_t publish_status = published;
 static dev_t dev;
 static struct cdev cdv;
-static struct class *cls 		= NULL;
-static volatile u32 *gpio_base 	= NULL;
+static struct class *cls         = NULL;
+static volatile u32 *gpio_base     = NULL;
 /* Timer */
 static struct hrtimer timer;
 
 static enum hrtimer_restart timer_handler(struct hrtimer *_timer)
 {
-	gpio_base[10] = 1 << 25;
-	return HRTIMER_NORESTART;
+    gpio_base[10] = 1 << 25;
+    return HRTIMER_NORESTART;
 }
 
 static void timer_set_handler(unsigned long time_s, unsigned long time_ns)
@@ -70,122 +70,122 @@ static void timer_set_handler(unsigned long time_s, unsigned long time_ns)
 
 void gpio_set(gpio_status_t _order)
 {
-	publish_status = edited;
-	switch (_order) {
-		case on:
-		gpio_base[7] = 1 << 25;
-		break;
+    publish_status = edited;
+    switch (_order) {
+        case on:
+        gpio_base[7] = 1 << 25;
+        break;
 
-		case off:
-		gpio_base[10] = 1 << 25;
-		break;
+        case off:
+        gpio_base[10] = 1 << 25;
+        break;
 
-		case pulse:
-		gpio_base[7] = 1 << 25;
-		timer_set_handler(0, 200000000);
-		break;
+        case pulse:
+        gpio_base[7] = 1 << 25;
+        timer_set_handler(0, 200000000);
+        break;
 
-		default:
-		publish_status = published;
-		break;
-	}
+        default:
+        publish_status = published;
+        break;
+    }
 }
 
 static ssize_t mcu_sigsend(struct file* filp, const char* buf, size_t count, loff_t *pos)
 {
-	char c;
-	if(copy_from_user(&c, buf, sizeof(char))) {
-		return -EFAULT;
-	}
+    char c;
+    if(copy_from_user(&c, buf, sizeof(char))) {
+        return -EFAULT;
+    }
 
-	// printk(KERN_INFO "recieve %c\n", c);
+    // printk(KERN_INFO "recieve %c\n", c);
 
-	switch (c) {
-		case '0':
-		gpio_set(off);
-		break;
+    switch (c) {
+        case '0':
+        gpio_set(off);
+        break;
 
-		case '1':
-		gpio_set(on);
-		break;
+        case '1':
+        gpio_set(on);
+        break;
 
-		case 'p':
-		gpio_set(pulse);
-		break;
+        case 'p':
+        gpio_set(pulse);
+        break;
 
-		default:
-		break;
-	}
+        default:
+        break;
+    }
 
-	return 1;
+    return 1;
 }
 
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t *pos)
 {
-	int size = 0;
-	const char msg_of_read[] = {0xF0, 0x9F, 0x8D, 0xA3 ,0x0A};
+    int size = 0;
+    const char msg_of_read[] = {0xF0, 0x9F, 0x8D, 0xA3 ,0x0A};
 
-	if (publish_status == edited) {
-		if (copy_to_user(buf + size, (const char *)msg_of_read, sizeof(msg_of_read))) {
-			printk(KERN_ERR "msg_of_read: copy_to_user failed.\n");
-			return -EFAULT;
-		}
-		publish_status = published;
-		printk(KERN_INFO "status changed to published.\n");
-	}
+    if (publish_status == edited) {
+        if (copy_to_user(buf + size, (const char *)msg_of_read, sizeof(msg_of_read))) {
+            printk(KERN_ERR "msg_of_read: copy_to_user failed.\n");
+            return -EFAULT;
+        }
+        publish_status = published;
+        printk(KERN_INFO "status changed to published.\n");
+    }
 
-	size += sizeof(msg_of_read);
-	return size;
+    size += sizeof(msg_of_read);
+    return size;
 }
 
 static struct file_operations led_fops = {
-	.owner = THIS_MODULE,
-	.write = mcu_sigsend,
-	.read  = sushi_read
+    .owner = THIS_MODULE,
+    .write = mcu_sigsend,
+    .read  = sushi_read
 };
 
 static int __init init_mod(void)
 {
-	int retval;
-	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
-	if (retval < 0) {
-		printk(KERN_ERR "alloc_chrdev_region failed.\n");
-		return retval;
-	}
+    int retval;
+    retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+    if (retval < 0) {
+        printk(KERN_ERR "alloc_chrdev_region failed.\n");
+        return retval;
+    }
     printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
 
-	cdev_init(&cdv, &led_fops);
-	retval = cdev_add(&cdv, dev, 1);
-	if (retval < 0) {
-		printk(KERN_ERR "cdev_add failed. major:%d, manor:%d\n", MAJOR(dev), MINOR(dev));
-	}
+    cdev_init(&cdv, &led_fops);
+    retval = cdev_add(&cdv, dev, 1);
+    if (retval < 0) {
+        printk(KERN_ERR "cdev_add failed. major:%d, manor:%d\n", MAJOR(dev), MINOR(dev));
+    }
 
-	cls = class_create(THIS_MODULE, "myled");
-	if (IS_ERR(cls)) {
-		printk(KERN_ERR, "class create failed.");
-		return PTR_ERR(cls);
-	}
+    cls = class_create(THIS_MODULE, "myled");
+    if (IS_ERR(cls)) {
+        printk(KERN_ERR, "class create failed.");
+        return PTR_ERR(cls);
+    }
 
-	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
-	
-	gpio_base = ioremap(RPI_REG_BASE, 0xA0);
+    device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
+    
+    gpio_base = ioremap(RPI_REG_BASE, 0xA0);
 
-	const u32 led 		= 25;
-	const u32 index 	= led / 10;
-	const u32 shift 	= (led%10)*3;
-	const u32 mask  	= ~(0x7 << shift);
-	gpio_base[index] 	= (gpio_base[index] & mask) | (0x1 << shift);
+    const u32 led       = 25;
+    const u32 index     = led / 10;
+    const u32 shift     = (led%10)*3;
+    const u32 mask      = ~(0x7 << shift);
+    gpio_base[index]    = (gpio_base[index] & mask) | (0x1 << shift);
 
-	return 0;
+    return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
-	cdev_del(&cdv);
-	device_destroy(cls, dev);
-	class_destroy(cls);
-	unregister_chrdev_region(dev, 1);
-	printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
+    cdev_del(&cdv);
+    device_destroy(cls, dev);
+    class_destroy(cls);
+    unregister_chrdev_region(dev, 1);
+    printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
 }
 
 module_init(init_mod);
